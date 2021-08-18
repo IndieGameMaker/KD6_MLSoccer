4d020d74e0de0b5a6422942861d9ef6cc1331586
Lee JaeHyun <myleje@gmail.com>
2021-08-18 13:24:26 +0900

    .

--- Assets/02.Scripts/BallCtrl.cs
+++ Assets/02.Scripts/BallCtrl.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
 using TMPro;
 
 public class BallCtrl : MonoBehaviour
 {
     public TMP_Text blueScoreText, redScoreText;
-
     private int blueScore, redScore;
 
-    // Start is called before the first frame update
+    public Agent[] players;
+    private Rigidbody rb;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitBall()
     {
-
+        rb.velocity = rb.angularVelocity = Vector3.zero;
+        transform.localPosition = new Vector3(0.0f, 1.5f, 0.0f);
     }
 }
