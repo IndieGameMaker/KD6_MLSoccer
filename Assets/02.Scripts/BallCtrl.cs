e2bb2076b92636465dfbef98b2e7dec402ba0cdf
Lee JaeHyun <myleje@gmail.com>
2021-08-18 13:20:47 +0900

    .

---
+++ Assets/02.Scripts/BallCtrl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BallCtrl : MonoBehaviour
+{
+    public TMP_Text blueScoreText, redScoreText;
+
+    private int blueScore, redScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
